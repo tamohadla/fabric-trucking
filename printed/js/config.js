1db221b89b3f31f89f1870b84e311dc02d7c5ea0
tamohadla <tamoh@live.com>
2025-12-18 23:58:20 +0200

    Update config.js

--- printed/js/config.js
+++ printed/js/config.js
@@ -1,5 +1,5 @@
 // إعدادات المشروع
-export const SUPABASE_URL = window.SUPABASE_URL || "";
-export const SUPABASE_ANON_KEY = window.SUPABASE_ANON_KEY || "";
+export const SUPABASE_URL = "https://umrczwoxjhxwvrezocrm.supabase.co";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVtcmN6d294amh4d3ZyZXpvY3JtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM5ODA0MTUsImV4cCI6MjA3OTU1NjQxNX0.88PDM2h93rhGhOxVRDa5q3rismemqJJEpmBdwWmfgVQ";
 export const TABLE_PRINTED = "printed_mariages";
 export const BUCKET_IMAGES = "images"; // اسم bucket في Supabase Storage

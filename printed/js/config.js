0d1502078ece82cc22f0e03b4551b142b450b1ad
tamohadla <tamoh@live.com>
2025-12-19 13:21:38 +0200

    Update config.js

--- printed/js/config.js
+++ printed/js/config.js
@@ -4,8 +4,8 @@
 
 const DEFAULT_CFG = {
   // Fill these two values from Supabase > Project Settings > API
-  SUPABASE_URL: "",
-  SUPABASE_ANON_KEY: "",
+  SUPABASE_URL: "https://umrczwoxjhxwvrezocrm.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVtcmN6d294amh4d3ZyZXpvY3JtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM5ODA0MTUsImV4cCI6MjA3OTU1NjQxNX0.88PDM2h93rhGhOxVRDa5q3rismemqJJEpmBdwWmfgVQ",
 
   // Database / Storage
   TABLE_NAME: "printed_mariages",
